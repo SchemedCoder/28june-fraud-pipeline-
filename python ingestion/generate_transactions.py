40252ece4a33ec19ef099687be9a29754e06c3d3
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-06-30 00:07:59 +0530

    Delete python ingestion directory

--- python ingestion/generate_transactions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-transactions = pd.read_csv("data/transactions.csv")
-customers = pd.read_csv("data/customers.csv")
-merchants = pd.read_csv("data/merchants.csv")
-
-print("Transactions:", transactions.shape)
-print("Customers:", customers.shape)
-print("Merchants:", merchants.shape)
